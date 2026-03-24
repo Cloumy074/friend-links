7cddfe11c41fb046051f521fd5ffb74e06b0c44b
Cloumy074 <Cloumy074@users.noreply.github.com>
2026-03-24 16:54:57 +0000

    deploy: f6bf0ac808256eb9857b8471c7d152b34027cabd

---
+++ output/linksData.mjs
@@ -0,0 +1,16 @@
+export const linksData = [
+  {
+    entries: [
+      {
+        avatar: "https://i.pinimg.com/736x/87/34/01/873401ee0dca96131f75c573bc18e6bc.jpg",
+        description: "Because of Passion",
+        screenshot: "",
+        title: "Happy7小七",
+        url: "https://site.happy7q233.net"
+      }
+    ],
+    group: "github",
+    groupDesc: "Some friends from GitHub! Might would like to take a look?",
+    groupName: "GitHub"
+  }
+];
